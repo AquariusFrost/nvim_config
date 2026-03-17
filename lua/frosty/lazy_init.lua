5d57b589cd03568342fc98e4326b897667ec36d5
AquariusFrost <frostaqua9@gmail.com>
2026-03-17 07:42:06 +0100

    disabled hererocks

--- lua/frosty/lazy_init.lua
+++ lua/frosty/lazy_init.lua
@@ -11,4 +11,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("frosty/lazy")
+require("lazy").setup("frosty/lazy", {
+    rocks = {
+        enabled = false,  -- Disable LuaRocks integration - no plugins require it
+                         -- This prevents hererocks build errors on Windows
+    },
+})
