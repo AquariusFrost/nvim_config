213c7be888c033b7c5f5577d1c10cde355b0372a
AquariusFrost <frostaqua9@gmail.com>
2026-03-17 13:45:11 +0100

    feat: added powershell as default for toggleterm under windows

--- lua/frosty/lazy/toggleterm.lua
+++ lua/frosty/lazy/toggleterm.lua
@@ -2,6 +2,21 @@ return {
 	'akinsho/toggleterm.nvim',
 	version = "*",
 	config = function()
+		-- Detect shell based on OS
+		local shell
+		if vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
+			-- Try PowerShell 7+ first, fallback to Windows PowerShell 5.1
+			if vim.fn.executable('pwsh') == 1 then
+				shell = 'pwsh.exe'
+			elseif vim.fn.executable('powershell') == 1 then
+				shell = 'powershell.exe'
+			else
+				shell = vim.o.shell -- Ultimate fallback
+			end
+		else
+			shell = vim.o.shell -- Default shell on Unix/Linux/Mac
+		end
+
 		require("toggleterm").setup({
 			-- Size configuration based on terminal direction
 			size = function(term)
@@ -24,7 +39,7 @@ return {
 			direction = 'float',
 			close_on_exit = true,
 			-- Auto-detect shell (bash/zsh on Unix, PowerShell on Windows)
-			shell = vim.o.shell,
+			shell = shell,
 			float_opts = {
 				border = 'curved',
 				winblend = 0,
@@ -33,12 +48,12 @@ return {
 
 		-- Easy escape from terminal mode to normal mode
 		vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { desc = "Exit terminal mode" })
-		
+
 		-- Numbered persistent terminals (horizontal splits)
 		vim.keymap.set('n', '<leader>t1', '<cmd>1ToggleTerm direction=horizontal<cr>', { desc = "Toggle terminal 1" })
 		vim.keymap.set('n', '<leader>t2', '<cmd>2ToggleTerm direction=horizontal<cr>', { desc = "Toggle terminal 2" })
 		vim.keymap.set('n', '<leader>t3', '<cmd>3ToggleTerm direction=horizontal<cr>', { desc = "Toggle terminal 3" })
-		
+
 		-- Explicit float toggle (same as <C-\> but using leader key)
 		vim.keymap.set('n', '<leader>tf', '<cmd>ToggleTerm direction=float<cr>', { desc = "Toggle floating terminal" })
 	end
